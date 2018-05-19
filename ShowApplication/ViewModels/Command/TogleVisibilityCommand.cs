a343a7803ab79e87d503051ce7bf0022f7f93102
Joan Lustenberger <lustenberger.joan@outlook.com>
2018-05-20 01:20:36 +0200

    Fixed Bug: When double Click on TrayIcon it didnt make a wakeup.

--- ShowApplication/ViewModels/Command/TogleVisibilityCommand.cs
+++ ShowApplication/ViewModels/Command/TogleVisibilityCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Windows;
+using System.Diagnostics;
+using System.Linq;
 using System.Windows.Input;
 
 namespace ShowApplication.ViewModels.Command
@@ -22,7 +23,29 @@ namespace ShowApplication.ViewModels.Command
             }
             else
             {
-               mainWindow.Show();
+                WakeUp(mainWindow);
+            }
+        }
+
+        internal void WakeUp(MainWindow mainWindow)
+        {
+            mainWindow.Show();
+            mainWindow.Activate();
+            PopulateSearchBox(mainWindow);
+        }
+
+        private void PopulateSearchBox(MainWindow mainWindow)
+        {
+            mainWindow.SearchBox.AutoSuggestionList.Clear();
+
+            mainWindow.SearchBox.Text = string.Empty;
+
+            foreach (Process item in Process.GetProcesses().Where(x => x.MainWindowHandle != IntPtr.Zero))
+            {
+                if (!mainWindow.SearchBox.AutoSuggestionList.Contains(item.ProcessName))
+                {
+                    mainWindow.SearchBox.AutoSuggestionList.Add(item.ProcessName);
+                }
             }
         }
     }
