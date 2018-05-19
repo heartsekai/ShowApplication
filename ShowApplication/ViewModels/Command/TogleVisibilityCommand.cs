5d191c53f51906ea389a85e582a514f5923ca3ec
Joan Lustenberger <lustenberger.joan@outlook.com>
2018-05-19 13:42:05 +0200

    Fixed namespace TogleVisibilityCommand

--- ShowApplication/ViewModels/Command/TogleVisibilityCommand.cs
+++ ShowApplication/ViewModels/Command/TogleVisibilityCommand.cs
@@ -2,7 +2,7 @@
 using System.Windows;
 using System.Windows.Input;
 
-namespace ShowApplication
+namespace ShowApplication.ViewModels.Command
 {
     public class TogleVisibilityCommand : ICommand
     {
